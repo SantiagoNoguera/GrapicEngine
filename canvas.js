eae2caf38cc89bf8f1dfd82915a9094a4b016094
SantiagoNoguera <santi.nog23@gmail.com>
2023-04-02 20:03:47 -0500

    Implementación del pintado de líneas con el algoritmo de bresenham.

--- canvas.js
+++ canvas.js
@@ -1,3 +1,80 @@
+/**
+ * 
+ * @param {Number} min Minimum value of the range.
+ * @param {Number} max Maximum value of the range.
+ * @returns {Number} Returns a random number that falls in the range.
+ */
+function random_number_between(min, max) {
+    const number = Math.random() * (max - min) + min;
+    return Math.floor(number);
+}
+
+/**
+ * Function to paint lines with Bresenham's line algorithm.
+ * @param {Number} x1 x-value of the fist point.
+ * @param {Number} y1 y-value of the fist point.
+ * @param {Number} x2 x-value of the second point.
+ * @param {Number} y2 y-value of the second point.
+ */
+function line_bresenham(x1, y1, x2, y2) {
+    var dx = x2 - x1, dy = y2 - y1;
+    var x, y;
+    var stepX, stepY;
+
+    //Determine the starting point and the end point on Y.
+    if (dy < 0) {
+        dy *= -1;
+        stepY = -1;
+    }
+    else {
+        stepY = 1;
+    }
+
+    //Determine the starting point and the end point on X.
+    if (dx < 0) {
+        dx *= -1;
+        stepX = -1;
+    }
+    else {
+        stepX = 1;
+    }
+
+    x = x1;
+    y = y1;
+
+    //Painting the line since the starting point.
+    var p;
+    if (dx > dy) {
+        p = 2 * dy - dx;
+        const twoDy = 2 * dy, twoDyMinusDx = 2 * (dy - dx);
+        while (x != x2) {
+            x += stepX;
+            if (p < 0) {
+                p += twoDy;
+            }
+            else {
+                y += stepY;
+                p += twoDyMinusDx;
+            }
+            context.fillRect(x, y, 1, 1);
+        }
+    }
+    else {
+        p = 2 * dx - dy;
+        const twoDx = 2 * dx, twoDxMinusDy = 2 * (dx - dy);
+        while (y != y2) {
+            y += stepY;
+            if (p < 0) {
+                p += twoDx;
+            }
+            else {
+                x += stepX;
+                p += twoDxMinusDy;
+            }
+            context.fillRect(x, y, 1, 1);
+        }
+    }
+}
 //Slection of the canvas.
 const canvas = document.querySelector('canvas');
 
@@ -5,23 +82,26 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-//Context of the canvas for 2D painting.
-const contex = canvas.getContext('2d');
+//contextt of the canvas for 2D painting.
+const context = canvas.getContext('2d');
 
 //Initialization of the painting area.
-contex.beginPath();
+context.beginPath();
 
-for(var i = 0; i < 100; i++) {
-    //Each point going to be paint every second.
+for (var i = 0; i < 10; i++) {
+    //Each line going to be paint every second.
     setTimeout(() => {
-        //Point position on X and Y.
-        var positionX = Math.floor(Math.random() * canvas.width);
-        var positionY = Math.floor(Math.random() * canvas.height);
+        //First point position on X and Y.
+        var x1 = Math.floor(Math.random() * canvas.width), y1 = Math.floor(Math.random() * canvas.height);
 
-        //Painting the point.
-        contex.fillRect(positionX, positionY, 10, 10);
+        //Second point position on X and Y.
+        var x2 = Math.floor(Math.random() * canvas.width), y2 = Math.floor(Math.random() * canvas.height);
+
+        //Painting the line.
+        context.fillStyle = 'rgb(' + random_number_between(0, 255) + ', ' + random_number_between(0, 255) + ', ' + random_number_between(0, 255) + ')';
+        line_bresenham(x1, y1, x2, y2);
     }, i * 1000);
 }
 
 //Closing of the painting area.
-contex.closePath();
+context.closePath();
